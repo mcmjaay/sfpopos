9274c8b3407741ec21fe707768593deac1631052
mcmjaay <114019418+mcmjaay@users.noreply.github.com>
2022-11-20 14:18:57 -0800

    title and subtitle

--- src/Title.js
+++ src/Title.js
@@ -1,10 +1,16 @@
 // src/Title.js
+import React from 'react'
+import './Title.css';
 
 function Title() {
   return (
-    <div>
+    <div className="Title" >
       <h1>SFPOPOS</h1>
+      <div className="Title-Subtitle">San Francisco Privately Owned Public Spaces</div>
+    <div className="Title-Subtitle">The one and only, Greatest of all Time...</div>
     </div>
+
+
   )
 }
 

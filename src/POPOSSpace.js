155142ab6fa7c46260e8bd952a57f2d1a247dda9
mcmjaay <114019418+mcmjaay@users.noreply.github.com>
2022-11-20 14:58:02 -0800

    images

--- src/POPOSSpace.js
+++ src/POPOSSpace.js
@@ -5,7 +5,7 @@ import logo from './logo.svg'
 function POPOSSpace() {
   return (
     <div>
-      <img src={`${process.env.PUBLIC_URL}/images/50-california-st.jpg`}
+      <img src={`${process.env.PUBLIC_URL}images/50-california-st.jpg`}
       width="300" height="300" alt="50 California St." />
       <h1>Name...</h1>
       <div>Address...</div>

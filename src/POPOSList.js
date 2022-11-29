52b78f3f6ebdaf7a10d2770f1c0f78fc62341ab3
mcmjaay <114019418+mcmjaay@users.noreply.github.com>
2022-11-29 10:59:41 -0800

    back to normal

--- src/POPOSList.js
+++ src/POPOSList.js
@@ -3,9 +3,21 @@ import POPOSSpace from './POPOSSpace';
 
 function POPOSList() {
   return (
-    <div>
+    <div className="POPOSList">
+      <POPOSSpace
+        name="50 California Street"
+        address="50 California St."
+        image="50-california-st.jpg"
+      />
+      <POPOSSpace />
+      <POPOSSpace />
+      <POPOSSpace />
+      <POPOSSpace />
+      <div>
       <h1>Keep your eye on this space for future content...</h1>
     </div>
+  </div>
+
   )
 }
 
--- src/POPOSSpace.js
+++ src/POPOSSpace.js
@@ -2,26 +2,15 @@
 
 import logo from './logo.svg'
 
-function POPOSSpace() {
-  return (
-    <div>
-      <img src={`${process.env.PUBLIC_URL}images/50-california-st.jpg`}
+function POPOSSpace(props) { // Add props here!
+  const { name, image, address } = props
+  < <div>
+      <img src={`${process.env.PUBLIC_URL}/images/50-california-st.jpg`}
       width="300" height="300" alt="50 California St." />
       <h1>Name...</h1>
       <div>Address...</div>
-    </div>
-  )
+</div>
+  
 }
 
 export default POPOSSpace
-
-function POPOSList() {
-  return (
-    <div>
-      <POPOSSpace />
-      <POPOSSpace />
-      <POPOSSpace />
-      <POPOSSpace />
-    </div>
-  )
-}
